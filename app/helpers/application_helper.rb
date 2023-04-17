a7470ad805f7ecb4e56f9c005ffe41a8f3c8f3ec
Gautam Gupta <ggupta78@gmail.com>
2023-04-17 16:55:38 +0530

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def full_page_title(page_title = '')
+    base_title = 'Sample App'
+    if page_title.empty?
+      base_title
+    else
+      # Interpolation would mess up html special chars
+      # "#{page_title} | #{base_title}"
+      page_title + ' | ' + base_title
+    end
+  end
 end
