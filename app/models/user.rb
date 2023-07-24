9b967eab45d5fc008ddb9e1d0837ee1d3eced452
Gautam Gupta <ggupta78@gmail.com>
2023-07-24 17:52:00 +0800

    remember me partially done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -16,5 +18,27 @@ class User < ApplicationRecord
             BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random 22 character token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Creates remember_digest using a new remember_token
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
