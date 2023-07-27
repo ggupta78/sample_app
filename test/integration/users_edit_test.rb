9da37c8d529f8ccd353ef1e70b57604765099ab4
Gautam Gupta <ggupta78@gmail.com>
2023-07-27 17:32:28 +0800

    complete updating users

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,44 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'unsuccessful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+        name: 'foo',
+        email: 'foo@example.com',
+        password: 'foo',
+        password_confirmation: 'foobar'
+      }
+    }
+    assert_template 'users/edit'
+    assert_select 'div.alert', {count: 1, text: 'The form contains 2 errors'}
+  end
+
+  test 'successful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: {
+      user: {
+        name: name,
+        email: email,
+        password: '',
+        password_confirmation: ''
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to(@user)
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+end
