a559b9774f9f2d31f00de1b5bb59a390f135e31e
Gautam Gupta <ggupta78@gmail.com>
2023-07-28 17:23:05 +0800

    complete destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # Create the main sample user
+puts 'Adding admin...'
 User.create!(name: 'Example User', email: 'example@railstutorial.org',
-             password: 'password', password_confirmation: 'password')
+             password: 'password', password_confirmation: 'password',
+             admin: true)
 
+puts 'Generating 99 other users...'
 # Generate 99 other users
 99.times do |n|
   name = Faker::Name.name
@@ -10,4 +13,5 @@ User.create!(name: 'Example User', email: 'example@railstutorial.org',
 
   User.create!(name: name, email: email, password: password,
                password_confirmation: password)
+  puts "User #{name} with email #{email} added!"
 end
