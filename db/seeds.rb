e2bfbbdb2a6e00b8983564e7023f4fe60bdc88d5
Gautam Gupta <ggupta78@gmail.com>
2023-08-05 17:54:40 +0800

    initial commit for account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,18 @@
 puts 'Adding admin...'
 User.create!(name: 'Example User', email: 'example@railstutorial.org',
              password: 'password', password_confirmation: 'password',
-             admin: true)
+             admin: true, activated: true, activated_at: Time.zone.now)
 
 puts 'Generating 99 other users...'
 # Generate 99 other users
 99.times do |n|
   name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n + 1}@railstutorial.org"
   password = 'password'
 
   User.create!(name: name, email: email, password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true, activated_at: Time.zone.now)
+
   puts "User #{name} with email #{email} added!"
 end
