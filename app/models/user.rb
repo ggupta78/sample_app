d677b60af211d950cbcc17ac4f48ce83bf85fb86
Gautam Gupta <ggupta78@gmail.com>
2023-09-26 15:59:23 +0530

    started followed-following feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,17 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-              format: { with: VALID_EMAIL_REGEX },
-              uniqueness: true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_many :microposts, dependent: :destroy
+
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
 
   # Returns the hash of a given string
   def self.digest(string)
