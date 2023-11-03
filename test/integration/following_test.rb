a793d342e9ae62d01e306fcfd16f4fc6234b2775
Gautam Gupta <ggupta78@gmail.com>
2023-11-03 11:50:52 +0800

    fix for html escaping test

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -29,12 +29,12 @@ class FollowPagesTest < Following
     end
   end
 
-  # test 'feed on Home page' do
-  #   get root_path
-  #   @user.feed.paginate(page: 1).each do |micropost|
-  #     assert_match CGI.escapeHTML(micropost.content), micropost.content
-  #   end
-  # end
+  test 'feed on Home page' do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
 end
 
 class FollowTest < Following
